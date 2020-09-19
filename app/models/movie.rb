ae4834b0bc0e510f3bfb0210c917c21723b8e7b5
Mike <mwhite7372@gmail.com>
2020-09-19 12:34:22 -0500

    Add today's homework

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
 
-  validates :year, :title, :plot, presence: true
+  validates :year, :title, :plot,presence: true
   validates :year, numericality: true
   validates :plot, length: {in: 10..500}
 
