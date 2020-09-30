adebe7ccf03a002901226a4791e4c285fbf49e53
Michael White <mwhite7372@gmail.com>
2020-09-29 21:07:05 -0500

    Added Create Movie_Genre entry

---
+++ app/controllers/api/movie_genres_controller.rb
@@ -0,0 +1,13 @@
+class Api::MovieGenresController < ApplicationController
+  def create
+    @moviegenre = MovieGenre.new(
+      movie_id: params[:movie_id],
+      genre_id: params[:genre_id],
+    )
+    if @moviegenre.save
+      render "show.json.jb"
+    else
+      render json: { message: @movie.errors.full_messages }, status: 422
+    end
+  end
+end
